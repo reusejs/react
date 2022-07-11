581adc14c5ea9675b419236012e8ee7430ba9ef9
Rajiv Seelam <rajiv@betalectic.com>
2022-07-11 18:27:54 +0530

    Rename to docs

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,11 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Cabinet Grotesk', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/typography')],
+}
