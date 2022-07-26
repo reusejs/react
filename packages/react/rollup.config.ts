d03ea5f549018abd85378c390e2b4a4adcff4cb7
Rajiv Seelam <rajiv@betalectic.com>
2022-07-26 13:17:27 +0530

    Add terser to minify js

--- packages/react/rollup.config.ts
+++ packages/react/rollup.config.ts
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
 export default {
@@ -34,5 +35,6 @@ export default {
     typescript({
       useTsconfigDeclarationDir: true,
     }),
+    terser(),
   ],
 };
