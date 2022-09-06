27f63787eb08bbe2f8738fb153b8638f43c09700
rajeshn95 <rajesh@betalectic.com>
2022-09-06 17:37:24 +0530

    lazy loading images horizontally

--- apps/storybook/src/stories/molecules/LazyLoadedImage.stories.tsx
+++ apps/storybook/src/stories/molecules/LazyLoadedImage.stories.tsx
@@ -10,6 +10,10 @@ export default {
 
 const abc: any = [
   'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2889&q=80',
+  'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2889&q=80',
+  'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2889&q=80',
+  'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2889&q=80',
+  'https://images.unsplash.com/photo-1598128558393-70ff21433be0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2889&q=80',
   'https://upload.wikimedia.org/wikipedia/commons/f/ff/Pizigani_1367_Chart_10MB.jpg',
   'https://images.unsplash.com/photo-1661859330852-8b34bb7a66c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2667&q=80',
   'https://images.unsplash.com/photo-1661846141704-0691950989e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80',
@@ -20,14 +24,6 @@ const abc: any = [
 ];
 const Template: ComponentStory<typeof LazyLoadedImage> = (args) => (
   <div className='flex flex-col'>
-    <div style={{ width: 200, height: 300 }}>
-      <div className='flex w-full animate-pulse space-x-4'>
-        <img
-          src='https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1'
-          className='object-cover'
-        />
-      </div>
-    </div>
     {abc.map((item: any, index: any) => {
       return <LazyLoadedImage {...args} src={item} key={index} />;
     })}
@@ -38,18 +34,30 @@ export const Default = Template.bind({});
 Default.args = {
   width: 200,
   height: 300,
-  placeholder: (
-    <img
-      src='https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1'
-      className='object-cover'
-    />
-  ),
-  // placeholderSrc:
-  //   'https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1',
-  // effect: 'blur',
-
-  // scrollPosition: { x: 0, y: 0 },
-  // visibleByDefault: false,
-  // delayMethod: 'debounce',
-  // delayTime: 120000,
+  placeholderSrc:
+    'https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1',
 };
+
+const HorizontalLazyLoading: ComponentStory<typeof LazyLoadedImage> = (
+  args
+) => (
+  <div className='gallery horizontal with-overflow'>
+    {abc.map((item: any, index: any) => {
+      return <LazyLoadedImage {...args} src={item} key={index} />;
+    })}
+  </div>
+);
+
+export const LazyLoadImagesInHorizontalDirection = HorizontalLazyLoading.bind(
+  {}
+);
+LazyLoadImagesInHorizontalDirection.args = {
+  width: 200,
+  height: 200,
+  placeholderSrc:
+    'https://i1.wp.com/www.slntechnologies.com/wp-content/uploads/2017/08/ef3-placeholder-image.jpg?ssl=1',
+  wrapperClassName: 'gallery-img-wrapper',
+};
+
+// placeholder as a component is ok, but when image is loading it won't show anything as the placeholder component is replaced by image
+// better way is use placeholderSrc, placeholderSrc is added img to background so that even while image is loading placeholder should be visible
