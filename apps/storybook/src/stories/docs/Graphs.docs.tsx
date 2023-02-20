62e6bdbd75f32188ef3139649a330660aa1f1bb1
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-02-20 12:16:47 +0530

    Added Docs and code fragments for each graph

---
+++ apps/storybook/src/stories/docs/Graphs.docs.tsx
@@ -0,0 +1,33 @@
+const docs = `
+This documentation will help you setup graphs(*\`more specifically, bar and line graphs\`*) in your react project. We will be using **react-chartjs-2** and **chartjs** to create graphs in our projects. This documentation will walk you through the step to :
+1. Setup Dependencies for using react-chartjs-2, 
+2. Give code fragments for examples of graphs
+3. Link to documentation to provide further help in customisation
+
+# Setup
+
+1. Following packages will be needed to be added to your dependencies
+    - **\`react-chartjs-2\`**
+    - **\`chart.js\`**
+    - **\`Faker\`** (if you want to populate graphs with some random data, **\`optional\`**)
+
+2. Run following commands to add individual packages:
+	For Chart.js and Reat-chartjs-2
+
+	> yarn add chart.js react-charjs.2 --save-dev
+
+	For Faker(only archived version is available as the creator have discontinued the package)
+
+	> yarn add faker@5.5.3 --save-dev
+
+
+# Further resource/documentation can be found at: 
+- https://www.chartjs.org/docs/latest/
+- https://react-chartjs-2.js.org/examples/line-chart
+- https://react-chartjs-2.js.org/components/bar
+
+> Code Fragment for each sample can be found commented in their respective code section or can be found in Reusejs's Git repository
+
+`;
+
+export default docs;
