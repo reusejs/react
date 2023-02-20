ee5e1104bcd31b6682cffc318ab00d56fc91ed09
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-02-20 09:10:47 +0530

    GRaphs Story Added

---
+++ apps/storybook/src/stories/molecules/LineGraph.stories.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default {
+  title: 'Templates/Line Graph',
+  argTypes: {},
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 2',
+      data: [30, 240, 300, 350, 520, 600, 728],
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export const LineGraph = () => <Line options={options} data={data} />;
