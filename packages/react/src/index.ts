55a9a435f8dd568b3d1e4d720f255929cb64c240
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-02-27 10:10:10 +0530

    Removed GraphBase from Reusejs components

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -120,7 +120,6 @@ export { default as DateInput } from "./molecules/datetime/dateInput";
 export { default as DateInputExample } from "./molecules/datetime/dateInputExample";
 export { default as HorizontalList } from "./molecules/horizontalList/base";
 export { default as IconList } from "./molecules/IconList/base";
-export { default as GraphBase } from "./molecules/graph/base";
 
 // ******************** End  ---- Molecules *************************
 
